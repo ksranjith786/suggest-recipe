e8d1c95912ec03d7322abfa01646a804ab70b9b0
ksranjith786 <ksranjith786@gmail.com>
2020-07-24 00:55:23 +0530

    updating config

--- project/app.py
+++ project/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, Blueprint, render_template
 from os import environ
 
-from config.config import app_config
-
 # Blueprints
 from routes.home import home_bp
 from routes.ingredients import ingredients_bp
@@ -22,9 +20,18 @@ def create_app():
 # end create_app
 
 def get_config(app):
-    config_name = environ.get('FLASK_ENV')
-    app.config.from_object(app_config[config_name])
-    #app.config.from_pyfile('config.py')
+    app.config.from_mapping(
+        SECRET_KEY='ranjithks',
+    )
+
+    app.config.from_pyfile('config.py', silent=True)
+    
+    envFLASK = environ.get('FLASK_ENV')
+
+    if envFLASK == 'development':
+        app.debug = True
+    else:
+        app.debug = False
     
 # end get_config
 
