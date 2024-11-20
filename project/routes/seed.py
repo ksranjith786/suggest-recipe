097a3548512e4c34a75d7ec14003dea993a6eaac
Ranjith KS <ksranjith786@gmail.com>
2024-11-20 12:12:25 +0530

    Update seed.py to read from csv row

--- project/routes/seed.py
+++ project/routes/seed.py
@@ -7,15 +7,16 @@ seed_bp = Blueprint('seed', __name__, url_prefix='/seed')
 @seed_bp.route('/recipes', methods=['GET'])
 def seedRecipes():
   with open("static/csv/recipes.csv", mode="r") as file:
-        data = csv.DictReader(file)
-        retVal = addRecipeToDB(
-                name = name,
-                url = url,
-                type = type,
-                imageURL = imageURL,
-                ingredients = str(ingredients), # Converting list to string
-                provider = provider
-            )
-        if retVal == False:
-            print("Exception caught while adding recipe details to Database")
-            return False
+    recipes = csv.DictReader(file)
+    for recipe in recipes:
+      retVal = addRecipeToDB(
+              name = recipe["name"],
+              url = recipe["url"],
+              type = recipe["type"],
+              imageURL = recipe["imageURL"],
+              ingredients = str(recipe["ingredients"]), # Converting list to string
+              provider = recipe["provider"]
+          )
+      if retVal == False:
+          print("Exception caught while adding recipe details to Database")
+          return False
