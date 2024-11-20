3877151ce46a3e41c6501e89dde6e6ba3a8a4371
Ranjith KS <ranjithks@Ranjiths-MacBook-Pro.local>
2024-11-20 21:24:49 +0530

    Updated class values while scraping the page content

--- project/sites/soup/findSites.py
+++ project/sites/soup/findSites.py
@@ -5,7 +5,7 @@ from sites.soup.ingredients import IngredientsExtractor
 from database.database import addRecipeToDB
 
 def run(url, provider):
-    print(url, ";", provider)
+    print(f'URL: {url} Provider: {provider}')
 
     url_response = None
     main_soup = None
@@ -13,47 +13,47 @@ def run(url, provider):
     try:
         url_response = requests.get(url).text
         main_soup = BeautifulSoup(url_response, 'html.parser')
-    except:
-        print("Exception caught while retrieving data from website")
+    except Exception as e:
+        print("Exception caught while retrieving data from website. Error: ", e)
         return False
     
-    #for link in soup.find_all('a'):
+    # for link in main_soup.find_all('a'):
     #    print(link.get('href'))
 
     """
     # Get the links that matches 'recipes'
-    for link in soup.find_all('a'):
+    for link in main_soup.find_all('a'):
         link_href = link.get('href')
         if "recipes" in link_href:
             print(link_href)
     """
     # Browse by Category
     BROWSE_BY_CATEGORIES = dict()
-    for div_cat in main_soup.find_all('div', {'class': "td-category-siblings"}):
-        #print('Div Category')
-        for ul in div_cat.findAll('ul', {'class': "td-category"}):
-            #print('Ul')
-            for list_item in ul.findAll('li', {'class': "entry-category"}):
-                #print('List Item')
-                #print(list_item.text)
-                for link in list_item.findAll('a'):
-                    #print('Link')
-                    #print(link.get('href'))
-                    name = ""
-                    for list_name in list_item.select_one('a'):
-                        name = list_name
+    for div_cat in main_soup.find_all('div', {'class': "menu-new-right-menu-container"}):
+        # print(f'Div Category: {div_cat}')
+        for ul in div_cat.findAll('ul', {'class': "td-mobile-main-menu"}):
+            # print(f'\n***\nul: {ul}')
+            for list_item in ul.findAll('li', {'class': "menu-item-has-children"}):
+                for sub_list_item in list_item.findAll('li', {'class': "menu-item"}):
+                    # print(f'\n***\nList Item: {sub_list_item.text}')
+                    for link in sub_list_item.findAll('a'):
+                        # print(f'\n***\nLink: {link.get('href')}')
+                        name = ""
+                        for list_name in sub_list_item.select_one('a'):
+                            name = list_name
 
-                    BROWSE_BY_CATEGORIES[name] = link.get('href')
-                    break
-            #print a.text.strip(), '=>', a.attrs['href']
-        #print(header_link.get('href'))
+                        BROWSE_BY_CATEGORIES[name] = link.get('href')
+                        break
+                    #print a.text.strip(), '=>', a.attrs['href']
 
-    #for k,v in BROWSE_BY_CATEGORIES.items():
-        #print(f'{k} => {v}')
+    # print("\n*** Links from Browse By Categories, Size:", len(BROWSE_BY_CATEGORIES))
+    # for k,v in BROWSE_BY_CATEGORIES.items():
+    #     print(f'{k} => {v}')
     
     RECIPE_LINKS = dict()
     RECIPE_PAGES = [page for page in BROWSE_BY_CATEGORIES.values()]
-    print("Fetching Recipe Pages")
+    RECIPE_PAGES.append("https://hebbarskitchen.com/recipes/top-paneer-recipes-paneer-curries")
+    # print("Fetching Recipe Pages")
     for PAGE_URL in RECIPE_PAGES:
         page_url_response = None
         page_soup = None
@@ -61,12 +61,13 @@ def run(url, provider):
         try:
             page_url_response = requests.get(PAGE_URL).text
             page_soup = BeautifulSoup(page_url_response, 'html.parser')
-        except:
-            print("Exception caught while retrieving data from website")
+        except Exception as e:
+            print("Exception caught while retrieving data from website. Error: ", e)
             return False
         # Get the Recipe Links from this page
-        print(PAGE_URL)
-        for div_con in page_soup.find_all('div', {'class': "td_module_19 td_module_wrap td-animation-stack"}):
+        # print(f'PAGE_URL: {PAGE_URL}')
+        for div_con in page_soup.find_all('div', {'class': "td-block-span6"}):
+            # print(f'Div Category: {div_con.text}')
             for link in div_con.findAll('a', {'class':"td-image-wrap"}):
                 RECIPE_NAME = link.get('title')
                 RECIPE_LINK = link.get('href')
@@ -76,7 +77,7 @@ def run(url, provider):
 
         # Get recipe pages
         for div_nav in page_soup('div', {'class': "page-nav td-pb-padding-side"}):
-            for link in div_nav.find_all('a', {'class': None}):
+            for link in div_nav.find_all('a', {'class': "page"}):
                 NEXT_PAGE = link.get('href')
                 # As there are PREVIOUS and NEXT buttons with out class <a>; let's skip if the previous link exists already
                 if NEXT_PAGE in RECIPE_PAGES:
@@ -89,7 +90,7 @@ def run(url, provider):
     # end for PAGE_URL
 
     print("No.of recipe links:", len(RECIPE_LINKS))
-    #print(RECIPE_LINKS)
+    # print(RECIPE_LINKS)
     print("Fetching Recipe Links")
     for RECIPE_LINK in RECIPE_LINKS.values():
         ingredientsExt = IngredientsExtractor(RECIPE_LINK, provider)
